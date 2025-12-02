048db65d741e17bea847a4b62d17fd840e2f85f0
Dbol07 <blackregulus.007subra@gmail.com>
2025-12-02 09:11:29 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import AppLayout from '@/components/AppLayout';
 import { AppProvider } from '@/contexts/AppContext';
 import { AuthProvider } from '@/contexts/AuthContext';
 import AuthWrapper from '@/components/AuthWrapper';
+import { Toaster } from 'react-hot-toast';
 
 const Index: React.FC = () => {
   return (
@@ -10,6 +11,8 @@ const Index: React.FC = () => {
       <AuthWrapper>
         <AppProvider>
           <AppLayout />
+          {/* Toast notifications available throughout the app */}
+          <Toaster position="top-right" />
         </AppProvider>
       </AuthWrapper>
     </AuthProvider>
