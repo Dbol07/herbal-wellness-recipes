662b4f89ca88891dbe3f31c23bcc912ee6b5da86
Dbol07 <blackregulus.007subra@gmail.com>
2025-12-03 01:31:29 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://nkbdoetlyufxobhqvkuf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rYmRvZXRseXVmeG9iaHF2a3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1ODk2MDYsImV4cCI6MjA4MDE2NTYwNn0.l9bUr-KOd8a5elH3Yb3rRJJIkasBcs7slNPGvYhKRbc';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
